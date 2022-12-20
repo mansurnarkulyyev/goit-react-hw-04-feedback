7803bef27c05dbf81c528a2ca1e4db6d5cf6114d
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-12-20 19:29:08 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,36 +3,12 @@ import PropTypes from 'prop-types';
 import st from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, vote }) => (
-  // const FeedbackOptions = ({ options, onBtnClick }) => (
   <div>
-    {/* <button className={st.Button} onClick={e => {
-      vote(e);
-    }}
-      name="good"
-      type="button"
-    > Good </button>
-    <button className={st.Button} onClick={e => {
-      vote(e);
-    }}
-      name="neutral"
-      type="button"
-    > Neutral </button>
-    <button className={st.Button} onClick={e => {
-      vote(e);
-    }}
-      name="bad"
-      type="button"
-    > Bad </button> */}
-
     {options.map(option => (
       <button className={st.Button} type="button" key={options} onClick={() => vote(option)}>
         {option}
       </button>
     ))}
-
-
-
-
   </div>
 );
 
